e574c9aafb434ee5a1eaa1dcd5a5d82e99eca7a0
Phillip Ridlen <phillip@ridlen.net>
2010-10-10 14:38:14 -0500

    abstract layout and css

--- application.rb
+++ application.rb
@@ -2,6 +2,6 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  response.headers['Cache-Control'] = "public, max-age=25200"
+  response.headers['Cache-Control'] = "public, max-age=2520"
   erb :index
 end
